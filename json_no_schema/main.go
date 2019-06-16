dcaebd078fc13b20a8d784741422f83a15b62b5b
markday1962 <mark@mark-day.me>
2019-06-16 11:48:45 +0100

    added walkJSON function

--- json_no_schema/main.go
+++ json_no_schema/main.go
@@ -27,9 +27,38 @@ func main() {
 	}
 	fmt.Println(f)
 	//map[address:[map[City:London HouseNumber:128a Postcode:E2 7RG Street:Columbia Road]] age:57 firstName:Mark lastName:Day]
-	printJSON(f)
+	simplePrintJSON(f)
+	walkJSON(f)
 }
 
-func printJSON(v interface{}) {
+func simplePrintJSON(v interface{}) {
 	fmt.Println(v)
+	m := v.(map[string]interface{})
+	// accessing top level data if we know the structure of v
+	fmt.Println(m["firstName"])
+	fmt.Println(m["lastName"])
+	fmt.Println(m["address"])
+}
+
+func walkJSON(v interface{}) {
+	switch vv := v.(type) {
+	case string:
+		fmt.Println("is a string", vv)
+	case float64:
+		fmt.Println("is a float64", vv)
+	case []interface{}:
+		fmt.Println("is an array", vv)
+		for k, v := range vv {
+			fmt.Println(k, " ")
+			walkJSON(v)
+		}
+	case map[string]interface{}:
+		fmt.Println("is an object", vv)
+		for k, v := range vv {
+			fmt.Println(k, " ")
+			walkJSON(v)
+		}
+	default:
+		fmt.Println("Type not defined")
+	}
 }
